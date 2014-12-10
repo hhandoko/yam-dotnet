04502e7a8893d279cc12f5fb4df93ad3f0375895
Herdy Handoko <herdy.handoko@hotmail.com>
2014-12-10 10:38:20 +0800

    Added Notification and Subscription clients. Updated library dependencies to the latest versions.

---
+++ src/main/YamNet.Core/Clients/NotificationClient.cs
@@ -0,0 +1,52 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="NotificationClient.cs" company="YamNet">
+//   Copyright (c) 2014 YamNet contributors
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace YamNet.Client
+{
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Yammer user's notification client.
+    /// </summary>
+    /// <remarks>
+    /// REST API documentation: https://developer.yammer.com/restapi/#rest-notifications
+    /// </remarks>
+    public class NotificationClient : ClientBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RelationshipClient"/> class.
+        /// </summary>
+        /// <param name="client">The client.</param>
+        public NotificationClient(Client client)
+            : base(client)
+        {
+        }
+
+        /// <summary>
+        /// Get current user's relationship.
+        /// </summary>
+        /// <returns>The <see cref="Relationship"/>.</returns>
+        public async Task<Relationship> Get()
+        {
+            var query = new RelationshipQuery(null, null);
+            var url = this.GetFinalUrl(string.Format("{0}.json", Endpoints.Relationships), query.SerializeQueryString());
+            var result = await this.Client.GetAsync<Relationship>(url);
+
+            return result.Content;
+        }
+    }
+
+    /// <summary>
+    /// The REST API endpoints.
+    /// </summary>
+    internal partial class Endpoints
+    {
+        /// <summary>
+        /// Notifications endpoint.
+        /// </summary>
+        public const string Notifications = "/streams/notifications";
+    }
+}
---
+++ src/main/YamNet.Core/Clients/SubscriptionClient.cs
@@ -0,0 +1,41 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="SubscriptionClient.cs" company="YamNet">
+//   Copyright (c) 2014 YamNet contributors
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace YamNet.Client
+{
+    using System;
+    using System.Net;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Yammer user's subscription client.
+    /// </summary>
+    /// <remarks>
+    /// REST API documentation: https://developer.yammer.com/restapi/#rest-notifications
+    /// </remarks>
+    public class SubscriptionClient : ClientBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SubscriptionClient"/> class.
+        /// </summary>
+        /// <param name="client">The client.</param>
+        public SubscriptionClient(Client client)
+            : base(client)
+        {
+        }
+    }
+
+    /// <summary>
+    /// The REST API endpoints.
+    /// </summary>
+    internal partial class Endpoints
+    {
+        /// <summary>
+        /// Notifications endpoint.
+        /// </summary>
+        public const string Subscriptions = "/subscriptions";
+    }
+}
